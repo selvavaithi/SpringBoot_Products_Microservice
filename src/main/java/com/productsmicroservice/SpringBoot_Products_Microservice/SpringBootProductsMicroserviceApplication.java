650d717b8d8aea02c99eedc77abb88819bd26e04
adeunqa_selva <selva.kumar@adeunqa.com>
2024-12-13 13:15:30 +0530

    my first commit

---
+++ src/main/java/com/productsmicroservice/SpringBoot_Products_Microservice/SpringBootProductsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.productsmicroservice.SpringBoot_Products_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productsmicroservice/SpringBoot_Products_Microservice/SpringBootProductsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productsmicroservice.SpringBoot_Products_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
